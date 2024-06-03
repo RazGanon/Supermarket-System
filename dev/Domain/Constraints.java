5bbfef927dbd79ffdc90038dd87056719f191fa8
NivSampson <129731047+NivSampson@users.noreply.github.com>
2024-06-03 20:06:06 +0300

    last comit for niv 03/06

--- dev/Domain/Constraints.java
+++ dev/Domain/Constraints.java
@@ -4,11 +4,33 @@ import java.util.ArrayList;
 
 
 public class Constraints
+
 {
+    private int[][] ConstraintsMatrix ;
+    public  Constraints (int [][] constraintsMatrix){
+        this.ConstraintsMatrix = constraintsMatrix;
+    }
 
-    String day_of_work;
-    String kind_of_shift;
+    //crate matrix of the shifts and day .
+    //column is thd day , 0 -day one , 1-day 2 ..
+    //row is the shifts - row 0 is morning shift and row 1 is evening shift.
+    //Each slot in the matrix have a number . 0 = cant work , 1 = can work.
+    public static int[][] buildMatrix() {
+        // Define the matrix dimensions
+        int rows = 2; // Two rows
+        int cols = 6; // Six columns
 
+        // Create the matrix
+        int[][] matrix = new int[rows][cols];
 
+        // Populate the matrix with zeros
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+
+        return matrix;
+}
 
 }
